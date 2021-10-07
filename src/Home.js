99a11a3c07e960d94df68f4290b094a6fbe72eef
segun <segzyguy@gmail.com>
2021-10-07 02:15:55 +0100

    added social icons on the header

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,10 @@
 import React from "react";
+import twitter from "../src/images/twitter.png";
+import facebook from "../src/images/facebook.png";
+import instagram from "../src/images/instagram.png";
+import gmail from "../src/images/gmail.png";
+import whatsapp from "../src/images/whatsapp.png";
+import phone from "../src/images/ringing-phone-icon.png";
 
 const Home = () => {
   return (
@@ -8,12 +14,31 @@ const Home = () => {
           {" "}
           <span>food hub</span>{" "}
         </h1>
+        <div className="header-icon">
+          <a href="#">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="#">
+            <img src={facebook} alt="facebook" />
+          </a>
+          <a href="#">
+            <img src={instagram} alt="instagram" />
+          </a>
+          <a href="#">
+            <img src={gmail} alt="gmail" />
+          </a>
+          <a href="#">
+            <img src={whatsapp} alt="whatsapp" />
+          </a>
+          <a href="#">
+            <img src={phone} alt="phone" className="phone-ring" />
+          </a>
+        </div>
 
-        <marquee behavior="scroll" direction="">
-          <h1>
-            welcome to <span>food hub</span>{" "}
-          </h1>
-        </marquee>
+        <h1>
+          welcome to <span>food hub</span>{" "}
+        </h1>
+        {/* <marquee behavior="scroll" direction=""></marquee> */}
       </div>
     </>
   );
